7f1a6f50684562f0137c525d82668e5856402f80
Izcar J. Muñoz Torrez <izcarmt95@gmail.com>
2023-12-16 13:48:24 -0600

    test: test

--- app/main.py
+++ app/main.py
@@ -1,26 +1,10 @@
-from typing import (
-    Any,
-    Dict,
-)
-
-from fastapi import (
-    FastAPI,
-)
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get(path='/healthcheck', description='Healthcheck endpoint', tags=['services'])
-async def healthCheck() -> Dict[str, Any]:
-    """
-    Esto es un healthcheck
-    (quiero texto duymmy,
-    quiero texto duymmy
-    quiero texto duymmy quiero texto duymmy,
-    quiero texto duymmy,
-    quiero texto duymmy quiero texto ,duymmy quiero texto duymmy quiero texto duymmy,)
-    """
-    caseMinor = 1
-    caseMinor += 1
-    caseMinor += 1
-    return {'status': 'ok'}
+async def healthcheck() -> dict[str, str]:
+    return {
+        'status': 'ok',
+    }
