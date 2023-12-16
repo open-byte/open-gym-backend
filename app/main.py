8f09ee5ac543e296885e80f27af657cd0aa18425
Izcar J. Muñoz Torrez <izcarmt95@gmail.com>
2023-12-15 21:33:26 -0600

    chore: add tools for linting

--- app/main.py
+++ app/main.py
@@ -1,26 +1,16 @@
-# standard library imports
-from typing import Any, Dict
-
-# third party imports
 from fastapi import FastAPI
+from typing import Any, Dict
 
 app = FastAPI()
 
 
-@app.get(
-    "/healthcheck",
-    description="Healthcheck endpoint",
-    tags=["services"],
-)
-async def healthcheck() -> Dict[
-    str,
-    Any,
-]:
+@app.get(path="/healthcheck", description="Healthcheck endpoint", tags=["services"])
+async def healthcheck() -> Dict[str, Any]:
     """
     Healthcheck endpoint
 
     Returns:
-        Dict[str, Any]: status with value `ok` if the service is up    asdfa sdfasd asfasd fasdf asdf asdfas fas fasf
+        Dict[str, Any]: status with value `ok` if the service is up s asdfa sdfasd asfasd fasdf asdf asdfas fas fasf
         asdf asfas fas fasd fas fasf asdf asdfasd f asfas fas fa
     """
     return {
